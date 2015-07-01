35b9feb54884ed1c26587945e6fa7a99aa153a07
Eric Larson <eric.larson@rackspace.com>
2015-07-01 11:26:34 -0500

    Get rid of an unused help print function

--- withenv/cli.py
+++ withenv/cli.py
@@ -12,24 +12,6 @@ from .args import parse_args
 from .env import compile
 
 
-def print_help_and_exit(*args):
-    help = '''usage: we [OPTIONS]... COMMAND
-
-Prepare the environment variables prior to running a command.
-
-    -e, --environment    use a specific YAML file
-    -d, --directory      recursively use a directory of YAML files
-    -a, --alias          use an alias YAML file for flags
-
-    -h, --help           display this message
-
-More than one flag can be used a time. Each flag will be applied to
-the environment variables in order, allowing a cascade of changes.
-'''
-    print(help)
-    sys.exit(0)
-
-
 def main():
     args = parse_args()
 
