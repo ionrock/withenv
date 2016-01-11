cbc0257c59ee13b239aa2fde08aff64018b0796d
Eric Larson <eric.larson@rackspace.com>
2016-01-11 10:46:58 -0600

    Bump version: 0.5.0 → 0.5.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requirements = [
 
 setup(
     name='withenv',
-    version='0.5.0',
+    version='0.5.1',
     description=('Prefix commands with specific environments specified '
                  'in YAML files.'),
     long_description=readme,
