996f74f85771a3f0c18923cdc594806e98de4e57
Eric Larson <eric.larson@rackspace.com>
2016-01-21 14:54:15 -0600

    Add test for catching ctrl-c

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,8 @@
+import time
 import subprocess
+import tempfile
+import signal
+
 
 from mock import patch, Mock
 
@@ -19,3 +23,15 @@ class TestMain(object):
     def test_we_return_code(self):
         assert subprocess.call('we echo'.split()) == 0
         assert subprocess.call('we eccho'.split()) == 127
+
+    def test_we_catches_ctrl_c(self):
+        fd, path = tempfile.mkstemp()
+        with open(path, 'wb+') as fh:
+            proc = subprocess.Popen(['we', 'sleep', '10'],
+                                    stdout=fh,
+                                    stderr=subprocess.STDOUT)
+            time.sleep(.5)
+            proc.send_signal(signal.SIGINT)
+            proc.wait()
+
+        assert open(path).read() == ''
